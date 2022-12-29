b97f0a1c0ee6027798dcb5f061900e5238a9de56
D22IT201 <120622708+D22IT201@users.noreply.github.com>
2022-12-29 10:10:24 +0530

    Add files via upload

---
+++ p1.js
@@ -0,0 +1,11 @@
+
+alert("hi");
+let a= parseInt(prompt("Enter 1 Number"));
+let b= parseInt(prompt("Enter 2 Number"));
+
+//et sum =0;
+ function calc(){
+    sum = a+b;
+ }
+//sum = a+b;
+//console.log(a+b);
